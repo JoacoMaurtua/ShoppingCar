e64355d6b952bc1a061e10ae04ca3d14ae27d73a
JoacoMaurtua <joacomaurtua@gmail.com>
2021-06-14 21:16:23 -0500

    the data is kept when reloading the page

---
+++ README.md
@@ -0,0 +1,5 @@
+# SHOPPING CART
+
+<!-- this is a simple project of a shopping cart storing products data persistently with localstorage, 
+I use purely html, css and jquery -->
+
--- script.js
+++ script.js
@@ -4,27 +4,27 @@ let products = [
     name:"Puma T-shirt",
     tag:'pumaTshirt',
     price: 35,
-    inCart:false
+    inCart:0
   },
   {
     name:"Red Nike T-shirt",
     tag:'redNikeTshirt',
     price: 35,
-    inCart:false
+    inCart:0
   },
   {
     name:"Black Nike T-shirt",
     tag:'blackNikeTshirt',
     price: 45,
-    inCart:false
+    inCart:0
   },
   {
     name:"Air Nike T-shirt",
     tag:'airNikeTshirt',
     price: 60,
-    inCart:false
+    inCart:0
   }
-]
+];
 
 for(let i = 0; i<carts.length; i++){
   carts[i].addEventListener('click',()=>{
@@ -32,11 +32,26 @@ for(let i = 0; i<carts.length; i++){
   });
 }
 
-const cartNumbers = () =>{
+const onLoadCartNumbers = () =>{ //mnatiene el contador de los productos al refrescar la pagina
   let productNumbers = localStorage.getItem('cartNumbers');
-  productNumbers = parseInt(productNumbers); //converir de cadena a numero
-
-  
-  localStorage.setItem('cartNumbers',1); //almacenar datos persistentes
+  if(productNumbers){
+    document.querySelector('.cart span').textContent = productNumbers;
+  }
 }
+
+const cartNumbers = () =>{
+    let productNumbers = localStorage.getItem('cartNumbers');
+    productNumbers = parseInt(productNumbers); //converir de cadena a numero
+    if(productNumbers){
+      localStorage.setItem('cartNumbers',productNumbers + 1); //almacenar datos persistentes
+      document.querySelector('.cart span').textContent = productNumbers + 1;
+    }
+    else{
+      localStorage.setItem('cartNumbers',1);
+      document.querySelector('.cart span').textContent = 1;
+    }
+
+}
+
+onLoadCartNumbers();
 
